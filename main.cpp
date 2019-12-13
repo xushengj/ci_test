d8f350da165d01cf29c9373e406f68ed9014c5ba
Shengjie(John) Xu <shengjie.xu@mail.utoronto.ca>
2019-12-12 22:11:26 -0500

    dummy change

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     bool isTest = parser.isSet(testOption);
     if(isTest){
         fprintf(stderr,"Test mode\n");
-        return 3;
+        return 4;
     }else{
         fprintf(stderr,"normal mode\n");
         return 1;
